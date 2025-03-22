53018b66688dd561befc46cbaf824f1eb0ec8444
Chandan Singh <Chandankapkoti04@gmail.com>
2025-03-22 14:42:24 +0530

    Delete SD.sql

--- SD.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE employees (
-    emp_id INT PRIMARY KEY NOT NULL,
-    emp_name VARCHAR(100) NOT NULL,
-    age INT CHECK (age >= 18),
-    email VARCHAR(255) UNIQUE,
-    salary DECIMAL DEFAULT 30000
-);
-
